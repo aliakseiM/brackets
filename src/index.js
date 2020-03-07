87dc1a02c0bd2a9825056148ad5b875d1badc000
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2020-03-08 01:01:08 +0300

    feat: complete 'brackets'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let cfg = bracketsConfig[i].join('');
+        if (str.includes(cfg)) {
+            str = str.replace(cfg, '');
+            i = -1;
+        }
+    }
+    return str.length === 0;
 }
